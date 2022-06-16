9b68324a54666e68478720376eeaa5de99e1a84d
Andrey <artist_hire@ukr.net>
2022-06-16 08:44:50 +0300

    Add hooks in Searchbar component

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,63 +1,54 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
 import toast from 'react-hot-toast';
 import { IoIosSearch } from 'react-icons/io';
 import { Container, Form, Button, Label, Input } from './Searchbar.styled';
 
-class Searchbar extends Component {
-  state = {
-    filter: '',
-  };
+function Searchbar({ onSubmit, isLoading }) {
+  const [filter, setFilter] = useState('');
+  const inputId = nanoid(5);
 
-  inputId = nanoid(5);
+  function handleChange({ target }) {
+    setFilter(target.value);
+  }
 
-  handleChange = ({ target }) => {
-    this.setState({ filter: target.value });
-  };
-
-  handleSubmit = evt => {
+  function handleSubmit(evt) {
     evt.preventDefault();
 
-    if (!this.props.isLoading) {
-      const normalizedFilter = this.state.filter.trim().toLowerCase();
-      this.setState({ filter: '' });
+    if (!isLoading) {
+      const normalizedFilter = filter.trim().toLowerCase();
+      setFilter('');
 
       if (normalizedFilter === '') {
         toast.error('Request cannot be empty');
         return;
       }
 
-      this.props.onSubmit(normalizedFilter);
+      onSubmit(normalizedFilter);
     }
-  };
-
-  render() {
-    return (
-      <Container>
-        <Form onSubmit={this.handleSubmit}>
-          <Button
-            type="submit"
-            aria-label="Search"
-            disabled={this.props.isLoading}
-          >
-            <IoIosSearch />
-          </Button>
-          <Label htmlFor={this.inputId}>Search images and photos</Label>
-          <Input
-            type="text"
-            autoComplete="off"
-            autoFocus
-            required
-            placeholder="Search images and photos"
-            id={this.inputId}
-            value={this.state.filter}
-            onChange={this.handleChange}
-          />
-        </Form>
-      </Container>
-    );
   }
+
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        <Button type="submit" aria-label="Search" disabled={isLoading}>
+          <IoIosSearch />
+        </Button>
+        <Label htmlFor={inputId}>Search images and photos</Label>
+        <Input
+          type="text"
+          autoComplete="off"
+          autoFocus
+          required
+          placeholder="Search images and photos"
+          id={inputId}
+          value={filter}
+          onChange={handleChange}
+        />
+      </Form>
+    </Container>
+  );
 }
 
 Searchbar.propTypes = {
